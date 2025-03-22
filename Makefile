566f91766107340134726db5d412f556efaf903b
elena_dobruk <elena.dobruk@yandex.ru>
2025-03-22 21:41:24 +0300

    set up database connection

--- Makefile
+++ Makefile
@@ -4,8 +4,14 @@ install:
 dev:
 	uv run python manage.py runserver
 
+migrations:
+	uv run python manage.py makemigrations
+
+migrate:
+	uv run python manage.py migrate
+
 build:
 	uv build
 
 lint:
-	uv run flake8 _
+	uv run flake8 api
--- wallet_app/settings.py
+++ wallet_app/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0mzsmh3j)(6wlugufe1wy&n8ex_yyh97vlx+l_h==q-y!-2^7l'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,12 +83,11 @@ WSGI_APPLICATION = 'wallet_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
