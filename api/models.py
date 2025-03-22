16457335c03e1a3e675529486ae3571a16dfe7e4
elena_dobruk <elena.dobruk@yandex.ru>
2025-03-22 21:48:21 +0300

    add Wallets model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Wallets(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False, unique=True)
+    balance = models.DecimalField(max_digits=15, decimal_places=3, default=0.000)
+
+    def __str__(self):
+        return f'Wallet {self.uuid}'
