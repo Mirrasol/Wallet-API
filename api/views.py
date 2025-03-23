1d5112435cdfffda7ce7f9571dc4682ccb18103e
elena_dobruk <elena.dobruk@yandex.ru>
2025-03-23 09:08:47 +0300

    add wallets view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from .models import Wallets
+from .repository import WalletsRepository
+from .serializers import WalletsSerializer
 
-# Create your views here.
+
+class WalletsViewSet(viewsets.ModelViewSet):
+    queryset = Wallets.objects.all()
+    serializer_class = WalletsSerializer
+    repository = WalletsRepository()
+    http_method_names = ['get', 'post']
+
+    @action(detail=True, methods=['post'])
+    def operation(self, request, pk=None):
+        data = request.data
+        wallet_uuid = pk
+        response = self.repository.conduct_operation(wallet_uuid, data)
+        return response
